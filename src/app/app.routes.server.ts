77c27714198c82372ef74392297684d9f4ea4bcb
TatianaPereyra <pereyratatiana175@gmail.com>
2026-06-13 00:15:12 -0300

    revert: volver al estado previo al deploy

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,10 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'producto/:id',
-    renderMode: RenderMode.Client
-  },
-  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
